98119cd3d24e6b45e082ea3e6817b63025ee9436
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-05-30 19:25:05 -0600

    HUD

--- scripts/dino.gd
+++ scripts/dino.gd
@@ -6,15 +6,19 @@ const JUMP_SPEED = -1800
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
-		$MoveCollision.disabled = false
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = JUMP_SPEED
-			$JumpSound.play()
-		elif Input.is_action_pressed("ui_down"):
-			$AnimatedSprite2D.play("Sneak")
-			$MoveCollision.disabled = true
+		# Get variable from parent 'game' to check if game running
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("Move")
+			$MoveCollision.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+				$JumpSound.play()
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("sneak")
+				$MoveCollision.disabled = true
+			else:
+				$AnimatedSprite2D.play("move")
 	else:
-		$AnimatedSprite2D.play("Jump")
+		$AnimatedSprite2D.play("jump")
 	move_and_slide()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,35 +1,51 @@
 extends Node
 
-const DINO_START_POS := Vector2i(28, 343)
+const DINO_START_POS := Vector2i(20, 343)
 const DINO_CAM_START_POS := Vector2i(213, 249)
 
-const START_SPEED = 10.0
-const MAX_SPEED = 25
+const START_SPEED = 5
+const MAX_SPEED = 20
+const SPEED_MODIFIER = 5000
+
+const SCORE_MODIFIER = 10
+
 var dino_speed
 
 var screen_size
 
 var score
 
+var game_running
 func _ready():
 	screen_size = get_window().size
 	new_game()
 	
-func _process(delta):
-	dino_speed = START_SPEED
+func _process(_delta):
+	if game_running:
+		dino_speed = START_SPEED * score / SPEED_MODIFIER
+		if dino_speed >= MAX_SPEED:
+			dino_speed = MAX_SPEED
+		
+		# Move dino and camera
+		$Dino.position.x += dino_speed
+		$DinoCamera.position.x += dino_speed
+		
+		# Update score
+		score += dino_speed
+		show_score()
+		
+		# Update ground position
+		# Camera has moved on too far and now the ground is about to go off
+		# the side of the screen.
+		if $DinoCamera.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			%HUD/StartLabel.visible = false
 	
-	# Move dino and camera
-	$Dino.position.x += dino_speed
-	$DinoCamera.position.x += dino_speed
-	
-	# Update score
-	score += dino_speed
-	
-	# Update ground position
-	# Camera has moved on too far and now the ground is about to go off
-	# the side of the screen.
-	if $DinoCamera.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+func show_score():
+	%HUD/ScoreLabel.text = "SCORE: " + str(score / SCORE_MODIFIER)
 	
 func new_game():
 	score = 0
@@ -37,3 +53,4 @@ func new_game():
 	$Dino.velocity = Vector2i(0, 0)
 	$DinoCamera.position = DINO_CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
+	%HUD/StartLabel.visible = true
