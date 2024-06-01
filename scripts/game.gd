0f1b6e0feb58781b36af990f370e9c96157186fd
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-06-01 17:33:07 -0600

    Music and Bug Fix

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,14 @@
 extends Node
 
+# Constants
+const DINO_START_POS := Vector2i(20, 350)
+const DINO_CAM_START_POS := Vector2i(256, 226)
+const START_SPEED : float = 5.0
+const MAX_SPEED : int = 20
+const SPEED_MODIFIER : int = 5000
+const SCORE_MODIFIER : int = 10
+const MAX_DIFFICULTY : int = 2
+
 # Obstacles
 var alien_scene = preload("res://scenes/alien.tscn")
 var bug_scene = preload("res://scenes/bug.tscn")
@@ -7,33 +16,18 @@ var mushroom_scene = preload("res://scenes/mushroom.tscn")
 var sign_scene = preload("res://scenes/sign.tscn")
 var stump_scene = preload("res://scenes/stump.tscn")
 var obstacle_types = [mushroom_scene, sign_scene, stump_scene]
-var obstacles : Array
 var bug_spawn_heights = [175, 275]
 
-const DINO_START_POS := Vector2i(20, 350)
-const DINO_CAM_START_POS := Vector2i(256, 226)
-
-const START_SPEED = 5
-const MAX_SPEED = 20
-const SPEED_MODIFIER = 5000
-
-const SCORE_MODIFIER = 10
-
-const MAX_DIFFICULTY = 2
-
-var dino_speed
-
-var screen_size
-var ground_height
-
-var score = 0
-var high_score = 0
-
-var game_running = false
-
+# Variables
+var obstacles : Array = []
+var dino_speed : float = START_SPEED
+var screen_size : Vector2i
+var ground_height : int
+var score : int = 0
+var high_score : int = 0
+var game_running : bool = false
 var last_obstacle
-
-var difficulty = 0
+var difficulty : int = 0
 
 func _ready():
 	screen_size = get_window().size
@@ -42,38 +36,44 @@ func _ready():
 	
 func _process(_delta):
 	if game_running:
-		dino_speed = START_SPEED + score / SPEED_MODIFIER
-		if dino_speed >= MAX_SPEED:
-			dino_speed = MAX_SPEED
-		
-		adjust_difficulty()
-		
-		# Generate obstacles
-		generate_obstacles()
-		
-		# Move dino and camera
-		$Dino.position.x += dino_speed
-		$DinoCamera.position.x += dino_speed
-		
-		# Update score
-		score += dino_speed
-		show_score()
-		
-		# Update ground position
-		# Camera has moved on too far and now the ground is about to go off
-		# the side of the screen.
-		if $DinoCamera.position.x - $Ground.position.x > screen_size.x * 1.5:
-			$Ground.position.x += screen_size.x
-			
-		# Clean up obstacles
-		for obstacle in obstacles:
-			if obstacle.position.x < ($DinoCamera.position.x - screen_size.x):
-				remove_obstacle(obstacle)
+		update_game()
 	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			%HUD/StartLabel.visible = false
 
+func update_game():
+	dino_speed = int(floor(START_SPEED) + floor(score) / floor(SPEED_MODIFIER))
+	if dino_speed >= MAX_SPEED:
+		dino_speed = MAX_SPEED
+	
+	adjust_difficulty()
+	print("difficulty adjusted...")
+	# Generate obstacles
+	generate_obstacles()
+	print("generated obstacle...")
+	
+	# Move dino and camera
+	$Dino.position.x += dino_speed
+	$DinoCamera.position.x += dino_speed
+	
+	# Update score
+	score += int(dino_speed)
+	show_score()
+	print("showing score...")
+	
+	# Update ground position
+	# Camera has moved on too far and now the ground is about to go off
+	# the side of the screen.
+	if $DinoCamera.position.x - $Ground.position.x > screen_size.x * 1.5:
+		$Ground.position.x += screen_size.x
+	print("adjusted ground...")
+	# Clean up obstacles
+	for obstacle in obstacles:
+		if obstacle.position.x < ($DinoCamera.position.x - screen_size.x):
+			remove_obstacle(obstacle)
+	print("clean up...")
+				
 func generate_obstacles():
 	# Generate ground obstacles
 	if obstacles.is_empty() or last_obstacle.position.x < score + randi_range(300, 500):
@@ -104,7 +104,7 @@ func add_obstacle(obstacle, x, y):
 	obstacles.append(obstacle)
 	
 func remove_obstacle(obstacle):
-	queue_free()
+	obstacle.queue_free()
 	obstacles.erase(obstacle)
 	
 func hit_obstacle(body):
@@ -112,7 +112,7 @@ func hit_obstacle(body):
 		game_over()
 		
 func show_score():
-	%HUD/ScoreLabel.text = "SCORE: " + str(score / SCORE_MODIFIER)
+	%HUD/ScoreLabel.text = "SCORE: " + str(int(floor(score) / floor(SCORE_MODIFIER)))
 
 # TODO: Add a game manager to save between reloads
 func check_high_score():
@@ -125,11 +125,12 @@ func new_game():
 	get_tree().reload_current_scene()
 	
 func adjust_difficulty():
-	difficulty = score / SPEED_MODIFIER
+	difficulty = int(floor(score) / floor(SPEED_MODIFIER))
 	if difficulty >= MAX_DIFFICULTY:
 		difficulty = MAX_DIFFICULTY
 
 func game_over():
+	$GameOverSound.play()
 	check_high_score()
 	get_tree().paused = true
 	game_running = false
